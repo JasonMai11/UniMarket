db10bd61664b41b7eb8a5facd3641d6738db7ffb
Jason Mai <maijason112102@gmail.com>
2025-05-21 10:39:31 -0400

    Initial Commit

---
+++ UniMarket/UniMarketApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniMarketApp.swift
+//  UniMarket
+//
+//  Created by Jason Mai on 5/21/25.
+//
+
+import SwiftUI
+
+@main
+struct UniMarketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UniMarketTests/UniMarketTests.swift
@@ -0,0 +1,17 @@
+//
+//  UniMarketTests.swift
+//  UniMarketTests
+//
+//  Created by Jason Mai on 5/21/25.
+//
+
+import Testing
+@testable import UniMarket
+
+struct UniMarketTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
